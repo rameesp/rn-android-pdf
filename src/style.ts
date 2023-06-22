4ff9b941d85e0bf7a3ba71beda5f863ddf6c0645
ramees_exampur <ramees@exampur.xyz>
2023-06-22 22:43:21 +0530

    style updated

--- src/style.ts
+++ src/style.ts
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'blue',
+    backgroundColor: 'white',
   },
   loaderContainer: {
     width: '100%',
